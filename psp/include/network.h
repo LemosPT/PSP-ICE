7c1dc30f46d63e55c117da4aef267b195a48cc96
Lemos <tomaslvieira13@gmail.com>
2026-09-08 18:38:56 +0100

    Add PSP network interface

---
+++ psp/include/network.h
@@ -0,0 +1,14 @@
+#ifndef PSP_ICE_NETWORK_H
+#define PSP_ICE_NETWORK_H
+
+#include <stdint.h>
+
+int ice_net_init(void);
+void ice_net_shutdown(void);
+int ice_net_socket(void);
+int ice_net_send(int sock, const char *host, uint16_t port,
+                const void *data, uint16_t length);
+int ice_net_receive(int sock, void *data, uint16_t capacity,
+                    uint32_t timeout_us);
+
+#endif
