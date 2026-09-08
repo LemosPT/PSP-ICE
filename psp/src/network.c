2c8b9d246e10aa271880d33b233c3951913da97b
Lemos <tomaslvieira13@gmail.com>
2026-09-08 20:23:28 +0100

    Added function & Header to call for Wi-Fi

--- psp/src/network.c
+++ psp/src/network.c
@@ -36,7 +36,7 @@ int ice_wifi_connect(int profile) {
     while (state != PSP_NET_APCTL_STATE_GOT_IP) {
         sceKernelDelayThread(1000000);
         sceNetApctlGetState(&state);
-        return -1;
+    return -1;
     }
 
     return -1;
