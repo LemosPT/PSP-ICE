d7f3c705f54d9a6cbb1a7b0b3e3870a18ddc29d0
Lemos <203381134+LemosPT@users.noreply.github.com>
2026-09-10 14:49:52 +0100

    Fixed Some lines of code

--- psp/src/network.c
+++ psp/src/network.c
@@ -69,6 +69,10 @@ int ice_net_init_stage(void) {
 int ice_wifi_connect(int profile) {
     // int profile = 0; // Default to the first profile
 
+    int ret;
+    int state;
+    int attempts = 0;
+
     SceUtilityNetconfData netconf;
     memset(&netconf, 0, sizeof(netconf));
     netconf.size = sizeof(netconf);
